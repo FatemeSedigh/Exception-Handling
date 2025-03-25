313f0c8de2b513b957eeca1730ea5094b41f7ad3
fati <fatemesadatsedigh@gmail.com>
2025-03-25 18:23:08 +0330

    writing-Exception

--- src/Exception.java
+++ src/Exception.java
@@ -1,2 +1,17 @@
-public class Exception {
+public class BookNotFoundException extends Exception {
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+}
+
+public class InvalidBookException extends Exception {
+    public InvalidBookException(String message) {
+        super(message);
+    }
+}
+
+public class EmptyLibraryException extends Exception {
+    public EmptyLibraryException(String message) {
+        super(message);
+    }
 }
